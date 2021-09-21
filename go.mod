74ef3f42eb4260e8d54df1add09c3f749191d645
Darshan-Josiah Barber <darshan@darshancomputing.com>
2021-09-21 01:44:06 -0700

    Experiment with relative changes (1 percentage point increase in brightness).  Switch to my fork of LIFX lan library to not sanitize kelvin to narrower range than bulb supports.

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require golang.org/x/sys v0.0.0-20210917161153-d61c044b1678
 
 require (
+	github.com/2tvenom/golifx v0.0.0-20181128083437-451b36557cb4 // indirect
+	github.com/darshan-/lifxlan v0.3.1-0.20210921082638-ed88e91c7a76 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.4.0 // indirect
@@ -13,6 +15,7 @@ require (
 	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rjeczalik/notify v0.9.2 // indirect
+	go.yhsif.com/lifxlan v0.3.0 // indirect
 	golang.org/x/term v0.0.0-20210916214954-140adaaadfaf // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- lan.go
+++ lan.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"sync"
 	"time"
 
-	"go.yhsif.com/lifxlan"
-	"go.yhsif.com/lifxlan/light"
+	"github.com/darshan-/lifxlan"
+	"github.com/darshan-/lifxlan/light"
+	//"go.yhsif.com/lifxlan"
+	//"go.yhsif.com/lifxlan/light"
 )
+
+// I think my net.Conn might sometimes go stale?
+// On error, maybe try redialing?  Even rediscovering device?
 
 func main() {
 	//ctx, cancel := context.WithCancel(context.Background())
@@ -33,7 +39,7 @@ func main() {
 		go func(device lifxlan.Device) {
 			defer wg.Done()
 			fmt.Println(device)
-			tryGetColor(device)
+			gotDevice(device)
 
 			// I'm not currently looking for more than one device; so just cancel once we get it
 			cancel()
@@ -43,19 +49,51 @@ func main() {
 	wg.Wait()
 }
 
-func tryGetColor(d lifxlan.Device) {
-	device, err  := light.Wrap(context.Background(), d, false)
+func gotDevice(d lifxlan.Device) {
+	conn, err := d.Dial()
+	if err != nil {
+		fmt.Println("Device.Dial() error:", err)
+		return
+	}
+
+	ld, err := light.Wrap(context.Background(), d, false)
 	if err != nil {
 		fmt.Println("light.Wrap error:", err)
 		return
 	}
+
+	color := getColor(ld, conn)
+	//fmt.Println(color)
+	fmt.Println("Hue       :", color.Hue)
+	fmt.Println("Saturation:", color.Saturation)
+	fmt.Println("Brightness:", color.Brightness)
+	fmt.Println("Kelvin    :", color.Kelvin)
+
+	color.Brightness += 655 // 2^16 / 100
+	setColor(ld, conn, color)
+}
+
+func getColor(d light.Device, conn net.Conn) *lifxlan.Color {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	color, err := device.GetColor(ctx, nil)
+	color, err := d.GetColor(ctx, conn)
 	if err != nil {
 		fmt.Println("GetColor error:", err)
+		return nil
+	}
+
+	return color
+}
+
+func setColor(d light.Device, conn net.Conn, color *lifxlan.Color) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err := d.SetColor(ctx, conn, color, 0, false)
+	if err != nil {
+		fmt.Println("SetColor error:", err)
 		return
 	}
 
-	fmt.Println(color)
+	fmt.Println("Set color:", color)
+	fmt.Println(getColor(d, conn))
 }
