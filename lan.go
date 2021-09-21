0d80eb221e17e3964b3d17b4b1c02b392092f1fb
Darshan-Josiah Barber <darshan@darshancomputing.com>
2021-09-21 02:21:08 -0700

    LAN power

--- lan.go
+++ lan.go
@@ -16,6 +16,12 @@ import (
 
 // I think my net.Conn might sometimes go stale?
 // On error, maybe try redialing?  Even rediscovering device?
+// I could maybe, on error, fail back to cloud API (for things that aren't relative, anyway [and I *could*
+//   parse json and have that full featured, too, just probably not worth it.  But I know it's 120 calls per
+//   minute, so probably fine, but this is faster, not limited (and limit my be reduced for cloud in future),
+//   and just feels more appropriate.  It'll work even if our internet service is down, or LIFX's cloud...])
+//   and then close conn and redial, so hopefully next action will work on lan.
+// Would it be worth pinging every few minutes?  Would that help keep it alive?
 
 func main() {
 	//ctx, cancel := context.WithCancel(context.Background())
@@ -55,6 +61,7 @@ func gotDevice(d lifxlan.Device) {
 		fmt.Println("Device.Dial() error:", err)
 		return
 	}
+	defer conn.Close() // Good idea?
 
 	ld, err := light.Wrap(context.Background(), d, false)
 	if err != nil {
@@ -62,15 +69,17 @@ func gotDevice(d lifxlan.Device) {
 		return
 	}
 
-	color := getColor(ld, conn)
-	//fmt.Println(color)
-	fmt.Println("Hue       :", color.Hue)
-	fmt.Println("Saturation:", color.Saturation)
-	fmt.Println("Brightness:", color.Brightness)
-	fmt.Println("Kelvin    :", color.Kelvin)
+	// color := getColor(ld, conn)
+	// //fmt.Println(color)
+	// fmt.Println("Hue       :", color.Hue)
+	// fmt.Println("Saturation:", color.Saturation)
+	// fmt.Println("Brightness:", color.Brightness)
+	// fmt.Println("Kelvin    :", color.Kelvin)
 
-	color.Brightness += 655 // 2^16 / 100
-	setColor(ld, conn, color)
+	// color.Brightness += 655 // 2^16 / 100
+	// setColor(ld, conn, color)
+
+	setPower(ld, conn, lifxlan.PowerOn)
 }
 
 func getColor(d light.Device, conn net.Conn) *lifxlan.Color {
@@ -96,4 +105,14 @@ func setColor(d light.Device, conn net.Conn, color *lifxlan.Color) {
 
 	fmt.Println("Set color:", color)
 	fmt.Println(getColor(d, conn))
+}
+
+func setPower(d light.Device, conn net.Conn, pow lifxlan.Power) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err := d.SetPower(ctx, conn, pow, false)
+	if err != nil {
+		fmt.Println("SetPower error:", err)
+		return
+	}
 }
