2f7c58d5cfed27ff334b11c4d192668fbf5d2e63
Darshan-Josiah Barber <darshan@darshancomputing.com>
2022-12-10 15:53:43 -0800

    Reading keyboard a different way, using /dev/hidraw0 (keys) and /dev/hidraw1 (dial), whose format is quite different, and no more fifo.  Hoping things'll work consistently now...

--- light-control.go
+++ light-control.go
@@ -24,14 +24,17 @@ Disable ttys and X
 
 const max_brightness = 65535
 const brightness_step = 2185 // 1/30 of range // 1966 // 3% of max
-const kelvin_step = 250 // 1/30 of range
+const kelvin_step = 250      // 1/30 of range
 
-var dev light.Device
-var conn net.Conn
+var (
+	dev  light.Device
+	conn net.Conn
+	quit = make(chan struct{})
+)
 
 func initLocalDevice() {
 	fmt.Println("initLocalDevice top")
-	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	deviceChan := make(chan lifxlan.Device)
@@ -112,7 +115,7 @@ func makeDimmer() {
 
 func makeBrighter() {
 	color := getColor()
-	if color.Brightness >= max_brightness - brightness_step {
+	if color.Brightness >= max_brightness-brightness_step {
 		color.Brightness = max_brightness
 	} else {
 		color.Brightness += brightness_step
@@ -122,7 +125,7 @@ func makeBrighter() {
 
 func makeWarmer() {
 	color := getColor()
-	if color.Kelvin <= lifxlan.KelvinMin + kelvin_step {
+	if color.Kelvin <= lifxlan.KelvinMin+kelvin_step {
 		color.Kelvin = lifxlan.KelvinMin
 	} else {
 		color.Kelvin -= kelvin_step
@@ -132,7 +135,7 @@ func makeWarmer() {
 
 func makeCooler() {
 	color := getColor()
-	if color.Kelvin >= lifxlan.KelvinMax - kelvin_step {
+	if color.Kelvin >= lifxlan.KelvinMax-kelvin_step {
 		color.Kelvin = lifxlan.KelvinMax
 	} else {
 		color.Kelvin += kelvin_step
@@ -162,9 +165,10 @@ func getPower() (pow lifxlan.Power) {
 }
 
 // Note -- with current cat /dev/input/event0 >>fifo& approach, if you accidentally do that more than once,
-//  you'll end up with 2 (or more) copies of each event.  If that's even, toggles won't work.  If it's greater
-//  than one, that'll move things like brightness by too much.  Let's do a script to remove and set up fifo on
-//  launch.
+//
+//	you'll end up with 2 (or more) copies of each event.  If that's even, toggles won't work.  If it's greater
+//	than one, that'll move things like brightness by too much.  Let's do a script to remove and set up fifo on
+//	launch.
 func togglePower() {
 	fmt.Println("togglePower")
 	if getPower() != lifxlan.PowerOn {
@@ -177,7 +181,7 @@ func togglePower() {
 const lifxStateUrl = "https://api.lifx.com/v1/lights/all/state"
 
 func power(state string) {
-	putReq("application/x-www-form-urlencoded", "power=" + state)
+	putReq("application/x-www-form-urlencoded", "power="+state)
 }
 
 func putJson(json string) {
@@ -197,7 +201,7 @@ func putReq(contentType, body string) {
 		return
 	}
 
-	req.Header.Add("Authorization", "Bearer " + lifx_token)
+	req.Header.Add("Authorization", "Bearer "+lifx_token)
 	req.Header.Set("Content-Type", contentType)
 
 	resp, err := http.DefaultClient.Do(req)
@@ -209,32 +213,14 @@ func putReq(contentType, body string) {
 }
 
 func main() {
-	f, err := os.Open("/home/pi/fifo")
-	if err != nil {
-		fmt.Printf("Error opening file: %v\n", err)
-		return
-	}
-	defer f.Close()
-
-	fmt.Println("Opened dev file")
-
-	// initLocalDevice()
-	// fmt.Println("initLocalDevice returned")
-
-	// sleepDur := 2 * time.Second
-	// while dev == nil  && sleepDur < 10 * time.Second {
-	// 	time.Sleep(sleepDur)
-	// 	sleepDur *= 2
-
-	// 	initLocalDevice()
-	// }
-
 	sleepDur := 2 * time.Second
-	for sleepDur <= 32 * time.Second {
+	for sleepDur <= 32*time.Second {
 		initLocalDevice()
 		fmt.Println("initLocalDevice returned")
 
-		if dev != nil { break }
+		if dev != nil {
+			break
+		}
 
 		fmt.Printf("dev is null, sleeping for %v\n", sleepDur)
 
@@ -248,6 +234,22 @@ func main() {
 		fmt.Println("Got lifx device!")
 	}
 
+	go keys()
+	go dial()
+
+	<-quit
+}
+
+func keys() {
+	f, err := os.Open("/dev/hidraw0")
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	fmt.Println("Opened /dev/hidraw0 for keys")
+
 	b := make([]byte, 16)
 
 	for {
@@ -256,63 +258,74 @@ func main() {
 			fmt.Printf("Error reading file: %v\n", err)
 			return
 		}
-		if n > 0 {
-			// 8 bytes for timestamp struct, then:
-			typ := uint16(b[8])
-			code := uint16(b[10])
-			value := uint16(b[12])
+		fmt.Printf("read %d bytes: %#v\n", n, b)
 
-			//fmt.Printf("%v, %v, %v\n", typ, code, value)
-			if typ == 1 { // Key event
-				if value == 1 { // key down
-					switch code {
-						// 1 key -> keycode2 and up by ones through 6 key -> keycode 7
-						// QWERT -> 16-20
-						// ASDFG -> 30-34
-						// ZXCVB -> 44-48
-						// TAB 15, CAPS 58, SHIFT 42, CTRL 29, ALT 56
-						// ESC 1, F1-F5 -> 59-63, `~ 41
-						//
-					case 1: // [ESC]
-						//power("off")
-						togglePower()
-					case 2: // [1]
-						setWhite(1500, 0.25)
-					case 3: // [2]
-						setWhite(2000, 0.35)
-					case 4: // [3]
-						setWhite(2700, 0.5)
-					case 5: // [4]
-						setWhite(3500, 0.75)
-					case 6: // [5]
-						setWhite(4300, 1)
-					case 7: // [6]
-						setWhite(5200, 1)
-					case 41: // [`]
-						//power("on")
-					case 114: // dial left
-						makeDimmer()
-					case 115: // dial right
-						makeBrighter()
-					case 62: // F4 (<<)
-						makeWarmer()
-					case 63: // F5 (>>)
-						makeCooler()
-					default:
-						fmt.Println("keycode:", code)
-					}
-				} else if value == 0 { // key up
-				} else if value == 2 { // autorepeat
-					switch code {
-					case 62: // F4 (<<)
-						makeWarmer()
-					case 63: // F5 (>>)
-						makeCooler()
-					}
-				}
-			}
-		} else {
-			fmt.Println("!")
+		switch b[2] {
+		case 0x29: // [ESC]
+			togglePower()
+		case 0x1e: // [1]
+			setWhite(1500, 0.25)
+		case 0x1f: // [2]
+			setWhite(2000, 0.35)
+		case 0x20: // [3]
+			setWhite(2700, 0.5)
+		case 0x21: // [4]
+			setWhite(3500, 0.75)
+		case 0x22: // [5]
+			setWhite(4300, 1)
+		case 0x23: // [6]
+			setWhite(5200, 1)
+		//case 0x35: // [`]
+		//power("on")
+		case 114: // dial left
+			makeDimmer()
+		case 115: // dial right
+			makeBrighter()
+		case 62: // F4 (<<)
+			makeWarmer()
+		case 63: // F5 (>>)
+			makeCooler()
+		case 0:
+			// ignore
+		default:
+			fmt.Println("keycode:", b[2])
+		}
+	}
+}
+
+func dial() {
+	f, err := os.Open("/dev/hidraw1")
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	fmt.Println("Opened /dev/hidraw1 for dial")
+
+	b := make([]byte, 16)
+
+	for {
+		n, err := f.Read(b)
+		if err != nil {
+			fmt.Printf("Error reading file: %v\n", err)
+			return
+		}
+		fmt.Printf("read %d bytes: %#v\n", n, b)
+
+		if b[0] != 0x3 {
+			continue
+		}
+
+		switch b[1] {
+		case 0xe9: // dial right
+			makeBrighter()
+		case 0xea: // dial left
+			makeDimmer()
+		case 0:
+			// ignore
+		default:
+			fmt.Println("keycode:", b[1])
 		}
 	}
 }
--- run-lc.sh
+++ run-lc.sh
@@ -2,10 +2,10 @@
 
 # Run with: `nohup ./run-lc.sh&`
 
-fifo="/home/pi/fifo"
+#fifo="/home/pi/fifo"
 
-rm $fifo
-mkfifo $fifo
-cat /dev/input/event0 >>$fifo&
-cat /dev/input/event1 >>$fifo&
-/home/pi/light-control
+#rm $fifo
+#mkfifo $fifo
+#cat /dev/input/event0 >>$fifo&
+#cat /dev/input/event1 >>$fifo&
+sudo /home/pi/light-control >>/home/pi/lc-logs
