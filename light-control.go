6b72c7d46dfb1349eab70e1a58d2a01f4835711a
Darshan-Josiah Barber <darshan@darshancomputing.com>
2022-12-14 23:47:38 -0800

    Made it awesome.

--- light-control.go
+++ light-control.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"reflect"
+	"runtime"
 	"time"
 
 	"github.com/darshan-/lifxlan"
@@ -29,7 +31,7 @@ var (
 )
 
 func initLocalDevice() {
-	log.Println("initLocalDevice...")
+	log.Println("Scanning for devices...")
 
 	var d lifxlan.Device
 
@@ -66,7 +68,7 @@ func initLocalDevice() {
 		log.Fatalf("light.Wrap error: %v", err)
 	}
 
-	log.Printf("Init complete!")
+	log.Printf("Initialization complete!")
 }
 
 func getColor(deadline time.Duration) *lifxlan.Color {
@@ -169,10 +171,6 @@ func getPower(deadline time.Duration) (pow lifxlan.Power) {
 	return
 }
 
-// Note -- with current cat /dev/input/event0 >>fifo& approach, if you accidentally do that more than once,
-// you'll end up with 2 (or more) copies of each event.  If that's even, toggles won't work.  If it's greater
-// than one, that'll move things like brightness by too much.  Let's do a script to remove and set up fifo on
-// launch.
 func togglePower() {
 	log.Println("togglePower")
 	if getPower(cmdDeadline) != lifxlan.PowerOn {
@@ -192,47 +190,14 @@ func main() {
 
 	initLocalDevice()
 
-	go keys()
-	go dial()
+	go handleinput("/dev/hidraw0", keys)
+	go handleinput("/dev/hidraw1", dial)
 	go keepAlive()
 
 	<-quit
 }
 
-// Exponential backoff worked, but I don't want to wait 15 seconds (1 + 2 + 4 + 8 (and then instantly work))
-// to turn on my freaking light, and it really seems like either the pi (my guess) or the light
-// (seems much less likely) is losing its connection.
-//
-// So I want to try regularly pinging the light with a getColor request and see if that resolves the issue
-
-// Keep alive seems to be working -- getting a lot of timeouts, but those seem to work right away on second
-// try, so so far seems to be keeping network active.
-
-// Finally googled ("braved") [raspberry pi wifi powers down] and found suggesion that wifi does power down,
-// lots of people complaining, and some folks saying you can check power save setting with:
-//
-//	iw dev wlan0 get power_save
-//
-// and set it with:
-//
-//	sudo iw dev wlan0 set power_save off
-//
-// Just checked, and that is reset to on when restarting the device.  So I want to check if I can quickly and
-// easily figure out how to set that as the default, but if not, the easy solution is to just turn it off in
-// start-up script.  If fact, that may actually be the best approach, because it keeps everything more self-
-// contained and easier to set up if/when I move to another device.  Yeah, I think I will just do that.
-//
-// It's crazy how mixed the reports are of that fixing things versus doing nothing, and some people claiming
-// with some authority that the value is reported incorrectly, but that the driver is hard-coded to never
-// power save.  So, we'll try this for now, and see.  Maybe it helps, and we won't see the timeouts for our
-// keepAlive getColor anymore, and everything will be perfect, which'd be great.  Or maybe it won't help, and
-// keepAlive will come to the rescue.  Or maybe neithe thing will work, and we'll be down to 15-second delays
-// sometimes, which would suck, but even *that* is an improvement over the status quo.  We'll see.
-
-// Huh, okay, so I was starting to ge my hopse up that wifi power management did the trick, but after 4 hours
-// and 15 minutes, we started getting a bunch of deadlines exceeded again.  But keepAlive does seem to be
-// compensating effectively, so for now switch still seems to work every time I use it.  I still don't like
-// this, because I could still get unlucky, and I hate that, but at least it's a lot better than it was...
+// If not closing connection realy does fix things, then let's get rid of this at some point...
 func keepAlive() {
 	for {
 		color := getColor(cmdDeadline)
@@ -244,15 +209,16 @@ func keepAlive() {
 	}
 }
 
-func keys() {
-	f, err := os.Open("/dev/hidraw0")
+func handleinput(dev string, handle func([]byte)) {
+	f, err := os.Open(dev)
 	if err != nil {
-		log.Printf("Error opening file: %v\n", err)
+		log.Printf("Error opening file '%s': %v\n", dev, err)
 		return
 	}
 	defer f.Close()
 
-	log.Println("Opened /dev/hidraw0 for keys")
+	handlerName := runtime.FuncForPC(reflect.ValueOf(handle).Pointer()).Name()[5:]
+	log.Printf("Opened %s with handler: %s", dev, handlerName)
 
 	b := make([]byte, 16)
 
@@ -262,68 +228,52 @@ func keys() {
 			log.Printf("Error reading file: %v\n", err)
 			return
 		}
-		log.Printf("read %d bytes: %#v\n", n, b)
+		log.Printf("read %d bytes: %d\n", n, b[:n])
 
-		switch b[2] {
-		case 0x29: // [ESC]
-			togglePower()
-		case 0x1e: // [1]
-			setWhite(1500, 0.25)
-		case 0x1f: // [2]
-			setWhite(2000, 0.35)
-		case 0x20: // [3]
-			setWhite(2700, 0.5)
-		case 0x21: // [4]
-			setWhite(3500, 0.75)
-		case 0x22: // [5]
-			setWhite(4300, 1)
-		case 0x23: // [6]
-			setWhite(5200, 1)
-		case 0x3d: // F4 (<<)
-			makeWarmer()
-		case 0x3e: // F5 (>>)
-			makeCooler()
-		case 0:
-			// ignore
-		default:
-			log.Println("keycode:", b[2])
-		}
+		handle(b)
 	}
 }
 
-func dial() {
-	f, err := os.Open("/dev/hidraw1")
-	if err != nil {
-		log.Printf("Error opening file: %v\n", err)
+func keys(k []byte) {
+	switch k[2] {
+	case 0x29: // [ESC]
+		togglePower()
+	case 0x1e: // [1]
+		setWhite(1500, 0.25)
+	case 0x1f: // [2]
+		setWhite(2000, 0.35)
+	case 0x20: // [3]
+		setWhite(2700, 0.5)
+	case 0x21: // [4]
+		setWhite(3500, 0.75)
+	case 0x22: // [5]
+		setWhite(4300, 1)
+	case 0x23: // [6]
+		setWhite(5200, 1)
+	case 0x3d: // F4 (<<)
+		makeWarmer()
+	case 0x3e: // F5 (>>)
+		makeCooler()
+	case 0:
+		// ignore
+	default:
+		log.Println("unhandled keycode:", k[2])
+	}
+}
+
+func dial(k []byte) {
+	if k[0] != 0x3 {
 		return
 	}
-	defer f.Close()
 
-	log.Println("Opened /dev/hidraw1 for dial")
-
-	b := make([]byte, 16)
-
-	for {
-		n, err := f.Read(b)
-		if err != nil {
-			log.Printf("Error reading file: %v\n", err)
-			return
-		}
-		log.Printf("read %d bytes: %#v\n", n, b)
-
-		if b[0] != 0x3 {
-			continue
-		}
-
-		switch b[1] {
-		case 0xe9: // dial right
-			makeBrighter()
-		case 0xea: // dial left
-			makeDimmer()
-		case 0:
-			// ignore
-		default:
-			log.Println("keycode:", b[1])
-		}
+	switch k[1] {
+	case 0xe9: // dial right
+		makeBrighter()
+	case 0xea: // dial left
+		makeDimmer()
+	case 0:
+		// ignore
+	default:
+		log.Println("keycode:", k[1])
 	}
 }
